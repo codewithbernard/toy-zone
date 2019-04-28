bac773968869fd6fdeab62677faa79c481f87bd2
Bernard Bado <bado.bernard94@gmailcom>
2019-04-28 17:42:52 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./index.css";
 
 const Text = () => {
-  return <p>Hello World!</p>;
+  return <p className="square">Hello World!</p>;
 };
 
 export default Text;
